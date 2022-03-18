dd28f2af2e671e36b87e958898a4ee70e934802a
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2022-03-18 19:29:26 +0530

    Add project files.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace todoApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
